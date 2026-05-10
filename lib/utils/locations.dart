49a18bdfef79406b9b26286114fe2e17316d3403
samreen-codify <ssamreen666789@gmail.com>
2026-05-10 17:18:25 +0500

    dropdown

---
+++ lib/utils/locations.dart
@@ -0,0 +1,24 @@
+class AppLocations {
+  static const List<String> locations = [
+    'Administration Block',
+    'Student Services Center (SSC)',
+    'Examination Block',
+    'Auditorium',
+    'E-Rozgaar Center',
+    'Academic Block A',
+    'Academic Block B',
+    'Al-Khwarizmi Block',
+    'Al-Farabi Block',
+    'Arfa Karim Block',
+    'Engineering Block',
+    'Law Block',
+    'Jabir Bin Hayyan Block',
+    'Sadequain Block',
+    'Nawaz Sharif Medical College',
+    'Quaid-e-Azam library',
+    'Iqbal Hall',
+    'Main cafe',
+    'P cafe',
+    'M cafe',
+  ];
+}
